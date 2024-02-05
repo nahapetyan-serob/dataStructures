51711150d05de62cc617c761933f3b2ff4db8046
Serob Nahapetyan <serobnahapetyan@Serob-MacBook-Pro.local>
2024-02-05 22:47:10 +0400

    adding maxHeap

---
+++ Heap/heap.py
@@ -0,0 +1,66 @@
+from typing import Union
+
+
+class Heap:
+    def __init__(self):
+        self.arr = []
+        self.heap_size = 0
+        self.top = self.arr[0] if self.heap_size > 0 else None
+
+    def get_right(self, i: int) -> int:
+        return 2 * i + 2
+
+    def get_left(self, i: int) -> int:
+        return 2 * i + 1
+
+    def swap(self, i, j) -> None:
+        self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
+
+    def max_heapify(self, index: int) -> None:
+        left = self.get_left(index)
+        right = self.get_right(index)
+        largest = index
+        if left <= self.heap_size and self.arr[left] > self.arr[largest]:
+            largest = left
+        if right <= self.heap_size and self.arr[right] > self.arr[largest]:
+            largest = right
+        if largest != index:
+            self.swap(largest, index)
+            self.max_heapify(largest)
+
+    def build_max_heap(self) -> None:
+        for i in range(self.heap_size // 2 - 1, -1, -1):
+            self.max_heapify(i)
+
+    def heap_sort(self) -> None:
+        self.build_max_heap()
+        for i in range(self.heap_size - 1, 0, -1):  # from the last element to second
+            self.swap(0, self.heap_size - 1)
+            self.heap_size -= 1
+            self.max_heapify(0)
+
+    def insert(self, value: int) -> None:
+        self.arr.append(value)
+        self.heap_size += 1
+        self.max_heapify(self.heap_size - 1)
+
+    def extract_max(self) -> Union[int, str]:
+        if self.heap_size < 1:
+            return 'Empty heap'
+        max_value = self.arr[0]
+        self.swap(0, self.heap_size - 1)
+        self.heap_size -= 1
+        self.max_heapify(0)
+        return max_value
+
+    def increase_key(self, index: int, value) -> None:
+        if index >= self.heap_size:
+            raise IndexError(f"Choose between 0 and {self.heap_size - 1}")
+        if self.arr[index] > value:
+            raise ValueError(f"Given value must be higher than {self.arr[index]}")
+        self.arr[index] = value
+        i = index
+        while i > 0 and self.arr[(i - 1) // 2] < self.arr[i]:
+            self.swap((i - 1) // 2, i)
+            i = (i - 1) // 2
+
